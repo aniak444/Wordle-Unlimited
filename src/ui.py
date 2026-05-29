7361a960352c4697dd68b25e68207147c3683419
Piotr Kobendzowski <pkobendzowski@gmail.com>
2026-05-29 16:46:14 +0200

    feat: configure dark-blue visual theme

--- src/ui.py
+++ src/ui.py
@@ -4,10 +4,13 @@ class WordleApp(ctk.CTk):
     def __init__(self):
         super().__init__()
 
+        # Task #2: Visual theme configuration
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
+
         self.title("Wordle Unlimited")
         self.geometry("450x700")
         self.resizable(False, False)
-        
 
 if __name__ == "__main__":
     app = WordleApp()
